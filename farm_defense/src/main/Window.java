7031e02e8044acc0d5c6084f3c13a712f0d746b4
Carl Gombert <gombe004@umn.edu>
2024-01-29 19:12:44 -0600

    edit window class

--- farm_defense/src/main/Window.java
+++ farm_defense/src/main/Window.java
@@ -13,15 +13,6 @@ import javax.swing.UIManager;
 
 public class Window {
 	
-	public static JButton startButton;
-	public static JButton stopButton;
-	public static JSlider gravitySlider;
-	public static JLabel gravityLabel;
-	public static JSlider velocitySlider;
-	public static JLabel velocityLabel;
-	public static JSlider angleSlider;
-	public static JLabel angleLabel;
-	
 	public Window(int width, int height, String title, Game game)
 	{
 		JFrame frame = new JFrame(title); 
