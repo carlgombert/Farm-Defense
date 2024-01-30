617c7262daf6b27ef8444c4644cb2f40a6d17546
Carl Gombert <gombe004@umn.edu>
2024-01-29 19:37:55 -0600

    implemented most of the boilerplate code

--- farm_defense/src/main/Game.java
+++ farm_defense/src/main/Game.java
@@ -3,22 +3,41 @@ package main;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public class Game extends Canvas implements Runnable{
+	
+	private static final long serialVersionUID = -5505267217615912489L;
 	
 	private Thread thread;
 	private boolean running = false;
 	public static final int WIDTH = 16 * 48, HEIGHT = 12 * 48;
 	
+	private Handler handler;
+	
+	public static Player player;
+	
 	public Game() {
+		handler = new Handler();
+		
+		player = new Player(ID.Player);
+		handler.addObject(player);
+		
+		this.addKeyListener(new KeyInput(player));
+		this.addMouseListener(new KeyInput(player));
 		
 		new Window(WIDTH, HEIGHT, "Zombie Valley", this);
 	}
 	
 	public static void main(String[] args) 
 	{
-		
+		new Game();
 	}
 	
 	public synchronized void start() {
@@ -66,10 +85,12 @@ public class Game extends Canvas implements Runnable{
 		stop();
 	}
 	
+	// tick method updates data of in game objects
 	private void tick() {
-		
+		handler.tick();
 	}
-
+	
+	// render method displays in game objects on the screen
 	private void render() {
 		BufferStrategy bs = this.getBufferStrategy();
 		if(bs == null) {
@@ -79,7 +100,20 @@ public class Game extends Canvas implements Runnable{
 		
 		Graphics g = bs.getDrawGraphics();
 		
+		handler.render(g);
+		
 		g.dispose();
 		bs.show();
+	}
+	
+	public static BufferedImage addImage(int width, int height, String file) {
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File(file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		return image;
 	}
 }
---
+++ farm_defense/src/main/GameObject.java
@@ -0,0 +1,94 @@
+package main;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+// superclass of all in game objects
+public abstract class GameObject {
+	
+	// object identifier
+	protected ID id;
+	
+	// world coordinates represent the position of the object on the broader map
+	protected int worldX, worldY;
+	protected int speedX, speedY;
+	// screen coordinates represent where the object should be rendered
+	protected int screenX, screenY;
+	
+	// the direction the object is facing
+	protected int direction = 0;
+	/*
+	 * front: 0
+	 * back: 1
+	 * left: 2
+	 * right: 3
+	 */
+	
+	public GameObject(int x, int y, ID id) {
+		this.worldX = x;
+		this.worldY = y;
+		this.id = id;
+	}
+	
+	public int getWorldX() {
+		return worldX;
+	}
+
+	public void setWorldX(int worldX) {
+		this.worldX = worldX;
+	}
+
+	public int getWorldY() {
+		return worldY;
+	}
+
+	public void setWorldY(int worldY) {
+		this.worldY = worldY;
+	}
+
+	public abstract void tick();
+	public abstract void render(Graphics g);
+	public abstract Rectangle getBounds();
+	
+	
+	public void setId(ID id) {
+		this.id = id;
+	}
+	public ID getId() {
+		return id;
+	}
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+	public int getSpeedX() {
+		return speedX;
+	}
+	public int getSpeedY() {
+		return speedY;
+	}
+	public int getDirection() {
+		return direction;
+	}
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+
+	public int getScreenX() {
+		return screenX;
+	}
+
+	public void setScreenX(int screenX) {
+		this.screenX = screenX;
+	}
+
+	public int getScreenY() {
+		return screenY;
+	}
+
+	public void setScreenY(int screenY) {
+		this.screenY = screenY;
+	}
+}
