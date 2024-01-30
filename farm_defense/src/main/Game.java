2b78efd578ed6229d546454872d03ecc258959d3
Carl Gombert <gombe004@umn.edu>
2024-01-29 18:52:57 -0600

    Implemented window and runtime algorithm

--- farm_defense/src/main/Game.java
+++ farm_defense/src/main/Game.java
@@ -1,9 +1,85 @@
 package main;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+	
+	private Thread thread;
+	private boolean running = false;
+	public static final int WIDTH = 16 * 48, HEIGHT = 12 * 48;
+	
+	public Game() {
+		
+		new Window(WIDTH, HEIGHT, "Zombie Valley", this);
+	}
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello world!");
+		
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				System.out.println("FPS: " + frames);
+				timer += 1000;
+				frames = 0;
+			}
+			
+		}
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.dispose();
+		bs.show();
 	}
 }
