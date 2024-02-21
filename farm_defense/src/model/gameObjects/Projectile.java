16cb9d45f2683cd8811e526b311486d2e645e0c5
Carl Gombert <gombe004@umn.edu>
2024-02-21 16:10:09 -0600

    added blood animation when bullet hits zombie

--- farm_defense/src/model/gameObjects/Projectile.java
+++ farm_defense/src/model/gameObjects/Projectile.java
@@ -2,12 +2,14 @@ package model.gameObjects;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 
 import controller.Game;
 import controller.objectHandling.ID;
 import model.GameObject;
+import util.ImageUtil;
 
 public class Projectile extends GameObject{
 		
@@ -15,9 +17,17 @@ public class Projectile extends GameObject{
 	private double angle;
 	private int speed = 20;
 	
-	private boolean hitzombie;
+	private boolean hitZombie = false; 
+	// hitZombie becomes true after projectile makes contact with zombie. this is used to start displaying
+	// blood animation
+	
+	private Image bloodAnimation = ImageUtil.addImage(16, 16, "resources/animations/blood.png");
+	//image for blood animation
 	
 	private LinkedList<Zombie> zombies;
+	
+	private int animationCount = 0;
+	// used to limit how long the animation stays on screen
 	
 	public Projectile(int x, int y, ID id, double angle) {
 		super(x, y, id);
@@ -26,46 +36,60 @@ public class Projectile extends GameObject{
 
 	public void tick() 
 	{
-		zombies = new LinkedList<Zombie>();
-		
-		for(int i = 0; i < Game.handler.object.size(); i++) 
-		{
-			GameObject tempObject = Game.handler.object.get(i);
-			if (tempObject.getId() == ID.Zombie) 
-			{
-				zombies.add((Zombie)tempObject);
-			}
-		}
-		
-		for (int j = 0; j < zombies.size(); j++)
-		{
-			Zombie tempZombie = zombies.get(j);
+		if(!hitZombie) { // If zombie has not been hit, continue to check for collisions and move bullet
+			zombies = new LinkedList<Zombie>();
 			
-			if(this.getBounds().intersects(tempZombie.getBounds()))
+			for(int i = 0; i < Game.handler.object.size(); i++) 
 			{
-					tempZombie.setHealth(tempZombie.getHealth() - 2.5);
-					
+				GameObject tempObject = Game.handler.object.get(i);
+				if (tempObject.getId() == ID.Zombie) 
+				{
+					zombies.add((Zombie)tempObject);
+				}
 			}
-		}
 		
-		worldX += (int) (speed * Math.cos(angle));
-		worldY += (int) (speed * Math.sin(angle));
+			for (int j = 0; j < zombies.size(); j++)
+			{
+				Zombie tempZombie = zombies.get(j);
+				
+				if(this.getBounds().intersects(tempZombie.getBounds()))
+				{
+					tempZombie.setHealth(tempZombie.getHealth() - 2.5);
+					hitZombie = true;
+				}
+			}
+		
+			worldX += (int) (speed * Math.cos(angle));
+			worldY += (int) (speed * Math.sin(angle));
+		}
 		setScreenX(worldX - Game.player.getWorldX() + Game.player.getScreenX());
 		setScreenY(worldY - Game.player.getWorldY() + Game.player.getScreenY());
 	}
 
 	public void render(Graphics g) {
-		if(worldX > Game.player.getWorldX() - Game.WIDTH &&
-				worldX < Game.player.getWorldX() + Game.WIDTH &&
-				worldY > Game.player.getWorldY() - Game.HEIGHT &&
-				worldY < Game.player.getWorldY() + Game.HEIGHT) {
-			g.setColor(Color.darkGray);
-			g.fillOval(getScreenX(), getScreenY(), 8, 8);
-		}
-		else {
-			Game.handler.removeObject(this);
+		if(!hitZombie) {
+			if(worldX > Game.player.getWorldX() - Game.WIDTH &&
+					worldX < Game.player.getWorldX() + Game.WIDTH &&
+					worldY > Game.player.getWorldY() - Game.HEIGHT &&
+					worldY < Game.player.getWorldY() + Game.HEIGHT) {
+				g.setColor(Color.darkGray);
+				g.fillOval(getScreenX(), getScreenY(), 8, 8);
+			}
+			else {
+				// If projectile leaves screen, remove it
+				Game.handler.removeObject(this);
+			}
 		}
 		
+		if(hitZombie) {
+			
+			g.drawImage(bloodAnimation, screenX-8, screenY-8, null);
+			animationCount++;
+			
+			if(animationCount > 50) {
+				Game.handler.removeObject(this);
+			}
+		}
 		
 	}
 
